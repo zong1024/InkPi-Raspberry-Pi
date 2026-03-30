1d0e23a4a83bb88c9dac35f93bf3b4e52ad1dcc6
zong1024 <zongrui0831@outlook.com>
2026-03-30 22:44:32 +0800

    prune obsolete data modules and synthetic dataset

--- data/camera/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-相机服务模块
-"""
-from data.camera.service import CameraService, CameraFrameListener
-
-__all__ = ["CameraService", "CameraFrameListener"]
--- data/preprocessing/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-图像预处理模块
-"""
-from data.preprocessing.service import PreprocessingService
-
-__all__ = ["PreprocessingService"]
